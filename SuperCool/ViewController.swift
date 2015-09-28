f8ff0f65bea31894d0ee20ee7c3fce58fa976858
Peter Lada <peter@easypairings.com>
2015-09-27 20:40:49 -0400

    exercise 1

--- SuperCool/ViewController.swift
+++ SuperCool/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var blueThing: UIImageView!
+    @IBOutlet weak var redThing: UIImageView!
+    
+    @IBOutlet weak var hideBlueButton: UIButton!
+    @IBOutlet weak var hideRedButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func hideTheBlueThing(sender: AnyObject) {
+        blueThing.hidden = true
+        redThing.hidden = false
+    }
 
+    @IBAction func hideTheRedThing(sender: AnyObject) {
+        blueThing.hidden = false
+        redThing.hidden = true
+    }
 }
 
